bfd8634abe35d368722fa70840f6a888fe48b776
Juta Mischenko-Slatenkova <juta.mischenko@gmail.com>
2020-06-15 23:21:38 +0300

    Recipes and shopping list tabs in header work appropriately.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'recipes-book';
+  tabSelected = 'recipes';
+
+  onNavigate(tab: string) {
+    this.tabSelected = tab;
+  }
+
 }
