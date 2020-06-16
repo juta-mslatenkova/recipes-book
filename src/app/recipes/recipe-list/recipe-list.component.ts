a02574952a93b4d31516d9f9e4a4a7db439da83a
Juta Mischenko-Slatenkova <juta.mischenko@gmail.com>
2020-06-16 23:21:56 +0300

    Recipes can be selected to show the details.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Recipe } from "../recipe.model";
-import { AppComponent } from "../../app.component";
 
 @Component({
   selector: 'app-recipe-list',
@@ -8,21 +7,27 @@ import { AppComponent } from "../../app.component";
   styleUrls: ['../../app.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+
   title1: string = "The first recipe";
   content1: string =  "The content";
-  imagePath1: string  = "https://dinnerthendessert.com/wp-content/uploads/2019/01/Belgian-Waffles-2-500x500.jpg";
-
+  imagePath1: string  = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT86U2zRmSeXtWCUXjLLNLaRMjgYpK6iCgPHmJBCJTy8or00oux&usqp=CAUg";
+  
   title2: string = "The second recipe";
   content2: string =  "The second content";
-  imagePath2: string  = "https://dinnerthendessert.com/wp-content/uploads/2019/01/Belgian-Waffles-2-500x500.jpg";
+  imagePath2: string  = "https://img.etimg.com/thumb/msid-75176755,width-640,resizemode-4,imgsize-612672/effect-of-coronavirus-on-food.jpg";
 
   recipes: Recipe[] = [
     new Recipe(this.title1, this.content1, this.imagePath1),
     new Recipe(this.title2, this.content2, this.imagePath2),
   ];
-
+  
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 }
