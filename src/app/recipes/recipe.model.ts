920562f3fd54e7aeb50fcdfc349df6841fba314d
Juta Mischenko-Slatenkova <juta.mischenko@gmail.com>
2020-06-23 14:21:46 +0300

    Ingredients added to the recipe details.

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,5 +1,12 @@
+import { Ingredient } from '../shopping-list/ingredient.model';
+
 export class Recipe {
-    constructor(public title: string, public content: string, public imagePath: string) {
+    
+    constructor(
+        public title: string, 
+        public content: string, 
+        public imagePath: string, 
+        public ingredients: Ingredient[]) {
     }
 
 }
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shopping-list/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
@@ -7,14 +8,16 @@ export class RecipeService {
     title1: string = "The first recipe";
     content1: string =  "The content";
     imagePath1: string  = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcT86U2zRmSeXtWCUXjLLNLaRMjgYpK6iCgPHmJBCJTy8or00oux&usqp=CAUg";
-    
+    ingredient1: Ingredient[] = [new Ingredient("bread", 1), new Ingredient ("apple", 2)];
+
     title2: string = "The second recipe";
     content2: string =  "The second content";
     imagePath2: string  = "https://img.etimg.com/thumb/msid-75176755,width-640,resizemode-4,imgsize-612672/effect-of-coronavirus-on-food.jpg";
+    ingredient2: Ingredient[] = [new Ingredient("juice", 1), new Ingredient ("water", 4)];
 
     private recipes: Recipe[] = [
-        new Recipe(this.title1, this.content1, this.imagePath1),
-        new Recipe(this.title2, this.content2, this.imagePath2),
+        new Recipe(this.title1, this.content1, this.imagePath1, this.ingredient1),
+        new Recipe(this.title2, this.content2, this.imagePath2, this.ingredient2),
     ];
 
     getRecipes() {
